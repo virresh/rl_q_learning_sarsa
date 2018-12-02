7ae61c4982ca9893c44b78afb097fa907ae04bdd
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-12-02 21:51:54 +0530

    Save Status A

---
+++ Agent.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pickle
+import math
+import types
+
+class Agent(object):
+    def __init__(self, n, m, action_space, load_file=None, update_epsilon=None, update_alpha=None):
+        self.rows = n
+        self.cols = m
+        if load_file:
+            self.Q = pickle.load(load_file)
+        else:
+            self.Q = np.zeros((n*m, action_space.n))
+        self.action_space = action_space
+        self.previous_state = None
+        self.epsilon = 0.8  # Exploration vs Exploitation
+        self.alpha = 0.8    # Learning rate
+        self.steps = 0
+        self.gamma = 0.99    # Reward Discount factor
+        if update_alpha:
+            self.update_alpha = types.MethodType(update_alpha, self)
+        if update_epsilon:
+            self.update_epsilon = types.MethodType(update_epsilon, self)
+
+    def convert_to_cell(self, state):
+        # state is a list of two numbers, x and y index
+        x,y = state
+        cell_num = x * self.rows + y
+        return int(cell_num)
+
+    def act(self, state, policy='eps_greedy'):
+        r = np.random.uniform(0,1)
+        # print(state, self.convert_to_cell(state))
+        state = self.convert_to_cell(state)
+        if policy == 'eps_greedy':
+            if r < self.epsilon:
+                # exploration
+                action = self.action_space.sample()
+            else:
+                # exploitation
+                action = int(np.argmax(self.Q[state,:]))
+        else:
+            # Always exploit
+            action = int(np.argmax(self.Q[state,:]))
+        self.previous_state = state
+        self.action_taken = action
+        return action
+
+    def learn(self, state, reward):
+        # print(state)
+        future = self.convert_to_cell(state)
+        # print(state, future, np.amax(self.Q[future]))
+        if self.previous_state  != None:
+            # We have a previous state, so we can learn
+            previous_val = self.Q[self.previous_state, self.action_taken]
+            target = reward + self.gamma * np.max(self.Q[future, :])
+            self.Q[self.previous_state, self.action_taken] += self.alpha * (target - previous_val)
+
+    def end_episode(self):
+        self.steps += 1
+        self.update_alpha()
+        self.update_epsilon()
+
+    def update_alpha(self):
+        self.alpha = max(self.alpha - 1/(self.steps+50), 0.3)
+
+    def update_epsilon(self):
+        self.epsilon = max(self.epsilon - 1/(self.steps+50), 0.01)
+
+    def save_table(self, file):
+        pickle.dump(self.Q, file)
---
+++ Runner.py
@@ -0,0 +1,209 @@
+import gym
+import gym_maze
+import matplotlib.pyplot as plt
+import numpy as np
+
+from Agent import Agent
+
+RENDER_MAZE = True
+
+TOTAL_REWARD = []
+TIME_TAKEN = []
+EPSILON_VALS = []
+ALPHA_VALS = []
+STATE_TREND1 = []
+STATE_TREND2 = []
+
+def main(agent_class, train=False, load_file=None, policy=None,
+		 EPOCHS=1000, ENV_STR='maze-sample-5x5-v0', MAX_T_MULT=100,
+		 upd_eps = None,
+		 upd_alpha = None,
+		 initial_eps = None,
+		 initial_alpha = None,
+		 initial_gamma = None,
+		 silent = False,
+		 state1 = None,
+		 state2 = None):
+	"""
+	Note : Put epochs = 1 for keeping things sane when using state1 and state2 variables
+	"""
+	global TOTAL_REWARD, TIME_TAKEN, EPSILON_VALS, ALPHA_VALS, STATE_TREND1, STATE_TREND2
+	TOTAL_REWARD = []
+	TIME_TAKEN = []
+	EPSILON_VALS = []
+	ALPHA_VALS = []
+	STATE_TREND1 = []
+	STATE_TREND2 = []
+
+	env = gym.make(ENV_STR)
+	# env = gym.make('maze-random-5x5-v0')
+	N = int(env.observation_space.high[0]) + 1
+
+	MAX_T = N*N*MAX_T_MULT
+	IDEAL_T = N*N
+
+	if load_file:
+		with open(load_file, 'rb') as f:
+			agent = agent_class(N, N, env.action_space, f, update_epsilon=upd_eps, update_alpha=upd_alpha)
+	else:
+		agent = agent_class(N, N, env.action_space, update_epsilon=upd_eps, update_alpha=upd_alpha)
+
+	if initial_alpha != None:
+		agent.alpha = initial_alpha
+	if initial_eps != None:
+		agent.epsilon = initial_eps
+	if initial_gamma != None:
+		agent.gamma = initial_gamma
+
+	# Do for these many epochs
+	for i in range(EPOCHS):
+		state = env.reset()
+		# env.render()
+		done = False
+		total_reward = 0
+		for t in range(MAX_T):
+			state, reward, done, _ = env.step(agent.act(state, policy))
+			total_reward += reward
+			if train:
+				agent.learn(state, reward)
+			if RENDER_MAZE:
+				env.render()
+			if done:
+				break
+		if not silent:
+			print('Episode', i, 'ended in ', t, 'time steps with ', total_reward, 'reward')
+		agent.end_episode()
+		if state1 != None:
+			STATE_TREND1.append(agent.Q[state1, :])
+			print(agent.Q[state1, :])
+		if state2 != None:
+			STATE_TREND2.append(agent.Q[state2, :])
+		TOTAL_REWARD.append(total_reward)
+		TIME_TAKEN.append(t)
+		EPSILON_VALS.append(agent.epsilon)
+		ALPHA_VALS.append(agent.alpha)
+
+	if train:
+		if load_file:
+			with open(load_file, 'wb') as f:
+				agent.save_table(f)
+		else:
+			with open('maze_agent.pkl', 'wb') as f:
+				agent.save_table(f)
+
+def plot_graph(title):
+	fig, axs = plt.subplots(2, 2, constrained_layout=True)
+	axs[0][0].plot(TIME_TAKEN)
+	axs[0][0].set_title('Number of steps taken')
+	axs[0][0].set_xlabel('Episodes')
+	axs[0][0].set_ylabel('Steps')
+
+	axs[0][1].plot(TOTAL_REWARD)
+	axs[0][1].set_title('Total reward')
+	axs[0][1].set_xlabel('Episodes')
+	axs[0][1].set_ylabel('Total Reward')
+
+	axs[1][0].plot(EPSILON_VALS)
+	axs[1][0].set_title('Epsilon value')
+	axs[1][0].set_xlabel('Episodes')
+	axs[1][0].set_ylabel('Epsilon')
+	
+	axs[1][1].plot(ALPHA_VALS)
+	axs[1][1].set_title('Alpha value')
+	axs[1][1].set_xlabel('Episodes')
+	axs[1][1].set_ylabel('Alpha')
+
+	fig.suptitle(title)
+	plt.show()
+
+def find_converge_time(values):
+	converged_val = min(values)
+	for i in range(10, len(values)):
+		found = True
+		for k in values[i-5:i]:
+			if k != converged_val:
+				found = False
+				break
+		if found:
+			return i-10
+	return len(values)
+
+if __name__ == '__main__':
+	RENDER_MAZE = False
+
+	def zero_eps(self):
+		self.epsilon = 0
+
+	def constant(self):
+		pass
+
+	## Part A
+	# # Agent with constant alpha update
+	# main(Agent, train=True, EPOCHS=50, MAX_T_MULT=8, upd_eps=zero_eps, upd_alpha=constant, initial_eps=0, policy=None)
+	# plot_graph('Q-Learning, Constant Alpha, No explore, Constant Gamma')
+
+	# # Agent with Episodic alpha decay and constant exploration
+	# main(Agent, train=True, EPOCHS=100, MAX_T_MULT=8, upd_eps=constant, policy='eps_greedy')
+	# plot_graph('Q-Learning, Episodic Alpha decay, Constant explore')
+
+	# # Agent with Episodic alpha decay and Episodic exploration decay
+	# main(Agent, train=True, EPOCHS=100, MAX_T_MULT=8, policy='eps_greedy')
+	# plot_graph('Q-Learning, Episodic Alpha decay, Episodic explore decay')
+
+	# # Agent with Episodic alpha decay
+	# main(Agent, train=True, EPOCHS=100, MAX_T_MULT=8, upd_eps=zero_eps, initial_eps=0)
+	# plot_graph('Q-Learning, Episodic Alpha decay, No explore')
+
+	## Part B impact of hyperparameters
+	# Effect of Gamma
+	# gamma_plot_list = []
+	# x = np.linspace(1, 0.0)
+	# for gamma_value in x:
+	# 	main(Agent, train=True, EPOCHS=50, MAX_T_MULT=8, policy=None, upd_eps=zero_eps, upd_alpha=constant, initial_gamma=gamma_value, silent=True)
+	# 	gamma_plot_list.append(find_converge_time(TIME_TAKEN))
+	# plt.figure()
+	# plt.plot(x, gamma_plot_list)
+	# plt.title('Convergence time vs Gamma')
+	# plt.show()
+	
+
+	# Effect of Alpha
+	# alpha_plot_list = []
+	# x = np.linspace(0.9, 0.0)
+	# for alpha_value in x:
+	# 	main(Agent, train=True, EPOCHS=50, MAX_T_MULT=8, policy=None, upd_alpha=constant, initial_alpha=alpha_value, silent=True)
+	# 	alpha_plot_list.append(find_converge_time(TIME_TAKEN))
+	# plt.figure()
+	# plt.plot(x, alpha_plot_list)
+	# plt.title('Convergence time vs Alpha')
+	# plt.show()
+
+	## Part C
+	# Observe probability of start state (i.e 0,0)
+	main(Agent, train=True, EPOCHS=50, MAX_T_MULT=8, upd_eps=zero_eps, state1=10, initial_eps=0)
+	# plot_graph('Q-Learning, Episodic Alpha decay, No explore')
+	arr = np.array(STATE_TREND1)
+	print(STATE_TREND1[0][0], arr[0][0])
+	print(arr.shape)
+	# print(arr)
+	fig, axs = plt.subplots(2,2, constrained_layout=True)
+	axs[0][0].plot(arr[:,0])
+	axs[0][1].plot(arr[:,1])
+	axs[1][0].plot(arr[:,2])
+	axs[1][1].plot(arr[:,3])
+	plt.show()
+
+	## Part D
+	# Effect of Espsilon
+	# epsilon_plot_list = []
+	# x = np.linspace(0.9, 0.0)
+	# for epsilon_value in x:
+	# 	main(Agent, train=True, EPOCHS=50, MAX_T_MULT=8, policy='eps_greedy', upd_eps=constant, initial_eps=epsilon_value, silent=True)
+	# 	epsilon_plot_list.append(find_converge_time(TIME_TAKEN))
+	# plt.figure()
+	# plt.plot(x, epsilon_plot_list)
+	# plt.title('Convergence time vs Epsilon')
+	# plt.show()
+
+	# main(Agent, train=True, load_file='maze_agent.pkl')
+	# main(Agent, train=False, load_file='maze_agent.pkl')
